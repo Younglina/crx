993a7b751cf8446e67763f9aa6d4229c804b116c
汪志强 <younglina0409@outlook.com>
2023-05-26 10:35:13 +0800

    feat: 账号

--- content-script.js
+++ content-script.js
@@ -4,10 +4,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request)
   var evt = new CustomEvent('input', { bubbles: true, cancelable: true })
 	const useradmin = document.querySelector("input[placeholder='请输入用户名']")
-  useradmin.value ='admin'
+  useradmin.value = request.name
   useradmin.dispatchEvent(evt)
 	const password = document.querySelector("input[placeholder='请输入密码']")
-  password.value = '2wsxVFR_'
+  password.value = request.password
   password.dispatchEvent(evt)
   document.querySelector("button").click()
 	sendResponse({name: 1});
--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
-let btn = document.getElementById("loginButton");
+let btns = document.getElementsByClassName("login-btn");
 // 获取当前tab标签
 const getCurrentTab = async () => {
  let queryOptions = {active: true, currentWindow: true};
  let [tab] = await chrome.tabs.query(queryOptions);
  return tab;
 };
- 
-btn.addEventListener("click", async () => {
-	const tab = await getCurrentTab();
-	await chrome.tabs.sendMessage(tab.id, {});
-});
+console.log(btns)
+for(let item of btns){
+	item.addEventListener("click", async () => {
+		console.log(item.dataset)
+		const tab = await getCurrentTab();
+		await chrome.tabs.sendMessage(tab.id, item.dataset);
+	});
+}
 
